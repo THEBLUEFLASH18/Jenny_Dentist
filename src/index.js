c0a927dc45f4f537514a793453d28f2f99610b7c
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2024-05-17 13:52:37 -0400

    Added all Necessary effects to all atropos elements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import Atropos from 'atropos';
 import 'atropos/atropos.css'; 
 
 // Initialize
+
+
 const myAtropos = Atropos({
   el: '.my-atropos',
+});
+
+document.querySelectorAll('.my-atropos').forEach(atroposElement => {
+  Atropos({
+    el: atroposElement,
+  });
 });
