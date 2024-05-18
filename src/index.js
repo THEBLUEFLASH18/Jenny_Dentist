1a562289dd4e30a881088b1027739acf28eb810b
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2024-05-18 18:35:55 -0400

    Added Spline integration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import Atropos from 'atropos';
 import 'atropos/atropos.css'; 
 import './styles.css'
+import { Application } from '@splinetool/runtime';
+
+
+
 
 // Initialize
 
+// Spline Canvas 
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/PNG2kyRn4x1rdnLt/scene.splinecode');
+
+
+// Atropos Settings 
 
 const myAtropos = Atropos({
   el: '.my-atropos',
