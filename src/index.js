3c6e4579d545729512c3683630c90f831b7188ac
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2024-05-23 12:40:55 -0400

    Added javascript configurations for the images so that webpack can add them to the dist directory

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 import Atropos from 'atropos';
 import 'atropos/atropos.css'; 
-import './styles.css'
+import './styles.css';
 import { Application } from '@splinetool/runtime';
 
+// Import images
+import jennyLogo from '/Images/Jenny_Logo.JPG';
+import dentalCleaningImage from '/Images/Dental-Cleaning.jpg';
+import rootCanalImage from '/Images/Root-Canal.jpg';
+import ortodontistImage from '/Images/Ortodontist.jpg';
+import dentalRemovableImage from '/Images/Dental_Removable.jpg';
+import dentalWhiteningImage from '/Images/Dental_Whitening.jpg';
+import fixedProthesesImage from '/Images/Fixed_Protheses.jpg';
 
-
-
-// Initialize
-
-// Spline Canvas 
+// Initialize Spline Canvas 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/PNG2kyRn4x1rdnLt/scene.splinecode');
 
-
-// Atropos Settings 
-
-const myAtropos = Atropos({
-  el: '.my-atropos',
-});
-
+// Initialize Atropos
 document.querySelectorAll('.my-atropos').forEach(atroposElement => {
   Atropos({
     el: atroposElement,
   });
 });
+
+// Set image sources dynamically
+document.querySelector('.logo img').src = jennyLogo;
+document.querySelectorAll('.atropos-inner')[0].querySelector('img').src = dentalCleaningImage;
+document.querySelectorAll('.atropos-inner')[1].querySelector('img').src = rootCanalImage;
+document.querySelectorAll('.atropos-inner')[2].querySelector('img').src = ortodontistImage;
+document.querySelectorAll('.atropos-inner')[3].querySelector('img').src = dentalRemovableImage;
+document.querySelectorAll('.atropos-inner')[4].querySelector('img').src = dentalWhiteningImage;
+document.querySelectorAll('.atropos-inner')[5].querySelector('img').src = fixedProthesesImage;
